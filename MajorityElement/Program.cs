3f15e90376fc7287d1e686948e978c5762b87282
Cristian Pisco <cristian.pisco@outlook.com>
2022-02-17 21:50:09 -0500

    MajorityElement code challenge from InterviewBit.com

---
+++ MajorityElement/Program.cs
@@ -0,0 +1,14 @@
+﻿static int majorityElement(List<int> A) {
+  if (A.Count() == 1) return A[0];
+  var dict = new Dictionary<int,int>();
+  foreach (var i in A)
+  {
+    if (dict.ContainsKey(i))
+      dict[i]++;
+    else
+      dict[i] = 1;
+  }
+  var res = dict.First(x => x.Value == dict.Values.Max()).Key;
+  return res;
+}
+majorityElement(new List<int>() { 1,1,2 });
